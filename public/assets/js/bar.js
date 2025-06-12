198c3fc4c568217cd62937c485c91c41865c81a4
PandoraMiracle <sakurahanna896@gmail.com>
2025-06-12 19:49:08 +0700

    Create algorithm-page

---
+++ public/assets/js/bar.js
@@ -0,0 +1,33 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.add('active');
+    document.getElementById('sidebar-overlay').classList.add('active');
+});
+
+document.getElementById('close-sidebar').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.remove('active');
+    document.getElementById('sidebar-overlay').classList.remove('active');
+});
+document.getElementById('sidebar-overlay').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.remove('active');
+    document.getElementById('sidebar-overlay').classList.remove('active');
+});
+
+document.getElementById('algorithms-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    const sublist = document.getElementById('algorithms-sublist');
+    sublist.style.display = (sublist.style.display === 'block') ? 'none' : 'block';
+});
+
+document.getElementById('searching-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    const sublist = document.getElementById('searching-sublist');
+    sublist.style.display = (sublist.style.display === 'block') ? 'none' : 'block';
+});
+
+document.querySelector('.home-icon').addEventListener('click', function() {
+    window.location.href = '/';
+});
+
+document.querySelector('.info-icon').addEventListener('click', function() {
+    window.location.href = '/about';
+});
