3de95577e5d9077328bb6c98a8b26094b1681202
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2025-06-12 22:30:13 +0700

    update

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-// Route cho trang chủ
 router.get('/', mainController.getHomePage);
-
-// Route cho trang giới thiệu
 router.get('/about', mainController.getAboutPage);
 
 module.exports = router;
